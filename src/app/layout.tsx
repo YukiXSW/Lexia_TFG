3fe8d201c3b23c1ee5213d8ef62f5799db4d1e86
Lili Wu <lilikw0212@gmail.com>
2026-05-14 23:33:26 +0200

    Es donde se renderiza todo el proyecto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col bg-white dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100 transition-colors">
+      <body className="h-full flex flex-col bg-white dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100 transition-colors">
         <Header />
-        <main className="flex-1 flex flex-col">
+        <main className="flex-1 flex flex-col min-h-0">
           {children}
         </main>
       </body>
